a9bb95ed8e42bc8f80bb112d773605e144830a01
caterina <catemarian1806@gmail.com>
2020-07-02 16:19:44 +0200

    compare e scompare il menu al click sulle icone

---
+++ main.js
@@ -0,0 +1,12 @@
+$(".fas.fa-bars").click( function () {
+    console.log("ok");
+    $(".hamburger-menu").show("fast");
+});
+
+  // function () {
+  //   $(".hamburger-menu.active").addClass("show");
+  // });
+  $(".fas.fa-times").click( function () {
+      console.log("ok2");
+      $(".hamburger-menu").hide("fast");
+  });
