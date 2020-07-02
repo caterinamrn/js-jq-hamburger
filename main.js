8c955103aaab85bb3ef4dcc41ea2a8cc02843163
caterina <catemarian1806@gmail.com>
2020-07-02 20:25:33 +0200

    anche se aperto il menu ad humburger scompare se supero la largezza di 1000px

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
-$(".fas.fa-bars").click( function () {
-    console.log("ok");
-    $(".hamburger-menu").show("fast");
-});
+// primo tentativo ma non considera la media query
 
-  // function () {
-  //   $(".hamburger-menu.active").addClass("show");
-  // });
-  $(".fas.fa-times").click( function () {
-      console.log("ok2");
-      $(".hamburger-menu").hide("fast");
+// $(".fas.fa-bars").click( function () {
+//     console.log("ok");
+//     $(".hamburger-menu").show("fast");
+// });
+//
+//   $(".fas.fa-times").click( function () {
+//       console.log("ok2");
+//       $(".hamburger-menu").hide("fast");
+//   });
+
+
+// secondo tentativo
+var x = window.matchMedia("(max-width: 1000px)")
+mediaQuery(x)
+x.addListener(mediaQuery)
+
+  $(".fas.fa-bars").click( function () {
+      console.log("ok");
+      $(".hamburger-menu").show("fast");
   });
+
+    $(".fas.fa-times").click( function () {
+        console.log("ok2");
+        $(".hamburger-menu").hide("fast");
+  });
+
+// functions
+function mediaQuery(x) {
+  if (x.matches) {
+    
+  }
+  else {
+    $(".hamburger-menu").hide();
+  }
+}
